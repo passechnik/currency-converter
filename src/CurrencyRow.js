c442b62b7c0f62dab9a9df5dc6cb9b0448c9c5be
Valerie Kucher <passechnik214@gmail.com>
2023-07-19 23:42:44 +0300

    implement api

--- src/CurrencyRow.js
+++ src/CurrencyRow.js
@@ -7,6 +7,21 @@ export default function CurrencyRow() {
         <select>
             <option value="Hi">Hi</option>
         </select>
+        <script>
+        async function getCurrency() {
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '95d73441f5msh110af27d713bf7ep1564e0jsn21dbe9dede91',
+            'X-RapidAPI-Host': 'currency-exchange.p.rapidapi.com'
+        }
+    }
+    const response = await fetch("https://currency-exchange.p.rapidapi.com/exchange?from=SGD&to=MYR&q=1.0", options);
+    const jsonData = await response.json();
+    console.log(jsonData)
+}
+getCurrency()
+        </script>
     </div>
     )
 }
